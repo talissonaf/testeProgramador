09fd535e3b4e6d560ab6827dec3a93c1ac20bcd1
talissonaf <web2.di.capitalo@unigran.br>
2020-04-28 14:35:05 -0400

    Primeiro 'commit'

---
+++ src/services/config.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const https = axios.create({
+    baseURL: 'https://www.unigran.br/campogrande/api/index.php/teste/'
+})
---
+++ src/services/tecnicos.js
@@ -0,0 +1,19 @@
+import { https } from './config'
+
+export default {
+    listar:() => {
+        return https.get('tecnico')
+    },
+
+    salvar:(tecnico) => {
+        return https.post('tecnico', tecnico)
+    },
+
+    atualizar:(tecnico) => {
+        return https.put('https://www.unigran.br/campogrande/api/index.php/teste/tecnico/' + tecnico.id, tecnico)
+    },
+
+    deletar:(tecnico) => {
+        return https.delete('https://www.unigran.br/campogrande/api/index.php/teste/tecnico/' + tecnico.id)
+    }
+}
